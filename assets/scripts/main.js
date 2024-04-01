da7338572e9fa8f0e0189bff529d832c5ba92cb0
Aksol Global <seidutesleem06@gmail.com>
2024-04-01 21:51:59 +0100

    styling landing page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -30,7 +30,7 @@
 // //   next.click();
 // // }, 3000);
 
-// // // 
+// // //
 // // clearInterval(refreshInterval);
 // // refreshInterval = setInterval(() => {
 // //     next.click();
@@ -55,59 +55,88 @@
 // //   });
 // // });
 
-
-
-
-let items = document.querySelectorAll('.slider .list .item');
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
-let thumbnails = document.querySelectorAll('.thumbnail .item');
+let items = document.querySelectorAll(".slider .list .item");
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
+let thumbnails = document.querySelectorAll(".thumbnail .item");
 
 // config param
 let countItem = items.length;
 let itemActive = 0;
 // event next click
-next.onclick = function(){
-    itemActive = itemActive + 1;
-    if(itemActive >= countItem){
-        itemActive = 0;
-    }
-    showSlider();
-}
+next.onclick = function () {
+  itemActive = itemActive + 1;
+  if (itemActive >= countItem) {
+    itemActive = 0;
+  }
+  showSlider();
+};
 //event prev click
-prev.onclick = function(){
-    itemActive = itemActive - 1;
-    if(itemActive < 0){
-        itemActive = countItem - 1;
-    }
-    showSlider();
-}
+prev.onclick = function () {
+  itemActive = itemActive - 1;
+  if (itemActive < 0) {
+    itemActive = countItem - 1;
+  }
+  showSlider();
+};
 // auto run slider
 let refreshInterval = setInterval(() => {
+  next.click();
+}, 5000);
+function showSlider() {
+  // remove item active old
+  let itemActiveOld = document.querySelector(".slider .list .item.active");
+  let thumbnailActiveOld = document.querySelector(".thumbnail .item.active");
+  itemActiveOld.classList.remove("active");
+  thumbnailActiveOld.classList.remove("active");
+
+  // active new item
+  items[itemActive].classList.add("active");
+  thumbnails[itemActive].classList.add("active");
+
+  // clear auto time run slider
+  clearInterval(refreshInterval);
+  refreshInterval = setInterval(() => {
     next.click();
-}, 5000)
-function showSlider(){
-    // remove item active old
-    let itemActiveOld = document.querySelector('.slider .list .item.active');
-    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
-    itemActiveOld.classList.remove('active');
-    thumbnailActiveOld.classList.remove('active');
-
-    // active new item
-    items[itemActive].classList.add('active');
-    thumbnails[itemActive].classList.add('active');
-
-    // clear auto time run slider
-    clearInterval(refreshInterval);
-    refreshInterval = setInterval(() => {
-        next.click();
-    }, 5000)
+  }, 5000);
 }
 
 // click thumbnail
 thumbnails.forEach((thumbnail, index) => {
-    thumbnail.addEventListener('click', () => {
-        itemActive = index;
-        showSlider();
-    })
-})
+  thumbnail.addEventListener("click", () => {
+    itemActive = index;
+    showSlider();
+  });
+});
+
+// Mission
+
+// Projects Slides
+$(".mission-slides").owlCarousel({
+  loop: true,
+  nav: true,
+  dots: false,
+  smartSpeed: 500,
+  margin: 25,
+  center: true,
+  autoplayHoverPause: true,
+  autoplay: true,
+  navText: [
+    "<i class='ri-arrow-left-s-line'></i>",
+    "<i class='ri-arrow-right-s-line'></i>",
+  ],
+  responsive: {
+    0: {
+      items: 1,
+    },
+    768: {
+      items: 2,
+    },
+    1024: {
+      items: 3,
+    },
+    1200: {
+      items: 4,
+    },
+  },
+});
